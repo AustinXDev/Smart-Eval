17f4d1ee0be188c9acd63b7fba8a57de2ad0ab4c
AustinxDev <dkingaustin@gmail.com>
2026-03-25 19:32:36 +0800

    creating a evaluation participation card UI

---
+++ views/admin/manage_teachers.view.php
@@ -0,0 +1,47 @@
+<?php 
+require_once __DIR__ . '/../../app/middleware/require_admin_auth.php';
+require_once __DIR__ . '/../../app/config/nav.php'; 
+require_once __DIR__ . '/../../app/helpers/session.php';
+
+if (isAdminLoggedIn()) {
+    $admin     = getAdmin();
+    $role      = strtolower(str_replace(' ', '_', $admin['role']));
+    $name      = $admin['username'];
+    $logout    = '/Smart-Eval/app/auth/logout.admin.php';
+} else {
+    $role      = 'student';
+    $studentID = getStudent();
+    $logout    = '/Smart-Eval/app/auth/logout.student.php';
+}
+
+$nav        = $navigation[$role] ?? [];
+$currentUrl = $_SERVER['REQUEST_URI'];
+
+$department = $_GET['dept'] ?? '';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Dashboard</title>
+
+  <?php include_once __DIR__ . '../../../public/assets/includes/head.php'?>
+
+  <!-- Custom CSS -->
+  <link rel="stylesheet" href="../../public/assets/css/custom.css">
+</head>
+<body>
+
+  <!-- header -->
+  <?php require __DIR__ . '/../partials/header.php'; ?>
+  
+  <!-- Sidebar -->
+  <?php require __DIR__ . '/../partials/sidebar.php'; ?>
+
+  <main class="pt-22 lg:ml-90 p-6 border-1 min-h-screen">
+    
+  </main>
+</body>
+</html>
--- views/pages/dashboard_content.php
+++ views/pages/dashboard_content.php
@@ -65,11 +65,11 @@
 </div>
 
 <!-- Ranking + Overall Score Distribution --->
-<div class="p-0 lg:p-5 min-h-screen mt-5">
+<div class="p-0 lg:p-5 mt-5">
 
   <div class="grid grid-cols-1 lg:grid-cols-2 gap-6">
 
-    <div class="lg:col-span-1 bg-white rounded-lg [box-shadow:rgba(0,0,0,0.02)_0px_1px_3px_0px,rgba(27,31,35,0.15)_0px_0px_0px_1px] p-5 hover:-translate-y-2 transition-all duration-300 hover:bg-gray-100">
+    <div class="lg:col-span-1 bg-white rounded-lg [box-shadow:rgba(0,0,0,0.02)_0px_1px_3px_0px,rgba(27,31,35,0.15)_0px_0px_0px_1px] p-5 hover:-translate-y-2 transition-all duration-300 hover:bg-gray-100  ">
       <!-- Ranking Table -->
 
       <div class="mb-4"> <!-- Header -->
@@ -152,6 +152,77 @@
           <h1 class="text-md lg:text-lg">Overall Score Distribution</h1>
         </div>
         <canvas id="scoreChart" class="w-full h-full"></canvas>
+    </div>
+  </div>
+
+</div>
+
+<!-- Student Participation Card Container-->
+<div class="p-5 grid grid-cols-3 gap-5 [box-shadow:rgba(0,0,0,0.02)_0px_1px_3px_0px,rgba(27,31,35,0.15)_0px_0px_0px_1px] rounded-sm mt-7">
+  <div class="flex flex-col gap-5 border-r-1 border-gray-200">
+    <div class="flex items-center gap-3 border-b-1 border-gray-200 pb-3">
+      <div class="bg-[#1A1A2E] p-3 rounded-sm">
+        <svg class="w-10 h-10" data-slot="icon" fill="none" stroke-width="1.5" stroke="#ffff" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M15 19.128a9.38 9.38 0 0 0 2.625.372 9.337 9.337 0 0 0 4.121-.952 4.125 4.125 0 0 0-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 0 1 8.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0 1 11.964-3.07M12 6.375a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0Zm8.25 2.25a2.625 2.625 0 1 1-5.25 0 2.625 2.625 0 0 1 5.25 0Z"></path>
+        </svg>
+      </div>
+      <p>Students Who Evaluated</p>
+    </div>
+    <div class="flex pr-5 items-center">
+      <div class="flex-1">
+        <span id="total" class="text-lg font-semibold lg:text-xl">200</span>
+        <span id="label">Total</span>
+      </div>
+      <span id="arrow">
+        <svg class="w-7 h-7" data-slot="icon" fill="none" stroke-width="1.5" stroke="green" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 10.5 12 3m0 0 7.5 7.5M12 3v18"></path>
+        </svg>
+      </span>
+    </div>
+  </div>
+
+  <div class="flex flex-col gap-5 border-r-1 border-gray-200">
+    <div class="flex items-center gap-3 border-b-1 border-gray-200 pb-3">
+      <div class="bg-[#1A1A2E] p-3 rounded-sm">
+        <svg class="w-10 h-10" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="red" class="size-6">
+        <path fill-rule="evenodd" d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25Zm-2.625 6c-.54 0-.828.419-.936.634a1.96 1.96 0 0 0-.189.866c0 .298.059.605.189.866.108.215.395.634.936.634.54 0 .828-.419.936-.634.13-.26.189-.568.189-.866 0-.298-.059-.605-.189-.866-.108-.215-.395-.634-.936-.634Zm4.314.634c.108-.215.395-.634.936-.634.54 0 .828.419.936.634.13.26.189.568.189.866 0 .298-.059.605-.189.866-.108.215-.395.634-.936.634-.54 0-.828-.419-.936-.634a1.96 1.96 0 0 1-.189-.866c0-.298.059-.605.189-.866Zm-4.34 7.964a.75.75 0 0 1-1.061-1.06 5.236 5.236 0 0 1 3.73-1.538 5.236 5.236 0 0 1 3.695 1.538.75.75 0 1 1-1.061 1.06 3.736 3.736 0 0 0-2.639-1.098 3.736 3.736 0 0 0-2.664 1.098Z" clip-rule="evenodd" />
+      </svg>
+      </div>
+      <p>Students Who Evaluated</p>
+    </div>
+    <div class="flex pr-5 items-center">
+      <div class="flex-1">
+        <span id="total" class="text-lg font-semibold lg:text-xl">200</span>
+        <span id="label">Total</span>
+      </div>
+      <span id="arrow">
+        <svg class="w-7 h-7" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="red" class="size-6">
+          <path fill-rule="evenodd" d="M12 2.25a.75.75 0 0 1 .75.75v16.19l6.22-6.22a.75.75 0 1 1 1.06 1.06l-7.5 7.5a.75.75 0 0 1-1.06 0l-7.5-7.5a.75.75 0 1 1 1.06-1.06l6.22 6.22V3a.75.75 0 0 1 .75-.75Z" clip-rule="evenodd" />
+        </svg>
+      </span>
+    </div>
+  </div>
+
+  <div class="flex flex-col gap-5 border-r-1 border-gray-200">
+    <div class="flex items-center gap-3 border-b-1 border-gray-200 pb-3">
+      <div class="bg-[#1A1A2E] p-3 rounded-sm">
+        <svg class="w-10 h-10" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#ffff" class="size-6">
+          <path fill-rule="evenodd" d="M7.502 6h7.128A3.375 3.375 0 0 1 18 9.375v9.375a3 3 0 0 0 3-3V6.108c0-1.505-1.125-2.811-2.664-2.94a48.972 48.972 0 0 0-.673-.05A3 3 0 0 0 15 1.5h-1.5a3 3 0 0 0-2.663 1.618c-.225.015-.45.032-.673.05C8.662 3.295 7.554 4.542 7.502 6ZM13.5 3A1.5 1.5 0 0 0 12 4.5h4.5A1.5 1.5 0 0 0 15 3h-1.5Z" clip-rule="evenodd" />
+          <path fill-rule="evenodd" d="M3 9.375C3 8.339 3.84 7.5 4.875 7.5h9.75c1.036 0 1.875.84 1.875 1.875v11.25c0 1.035-.84 1.875-1.875 1.875h-9.75A1.875 1.875 0 0 1 3 20.625V9.375ZM6 12a.75.75 0 0 1 .75-.75h.008a.75.75 0 0 1 .75.75v.008a.75.75 0 0 1-.75.75H6.75a.75.75 0 0 1-.75-.75V12Zm2.25 0a.75.75 0 0 1 .75-.75h3.75a.75.75 0 0 1 0 1.5H9a.75.75 0 0 1-.75-.75ZM6 15a.75.75 0 0 1 .75-.75h.008a.75.75 0 0 1 .75.75v.008a.75.75 0 0 1-.75.75H6.75a.75.75 0 0 1-.75-.75V15Zm2.25 0a.75.75 0 0 1 .75-.75h3.75a.75.75 0 0 1 0 1.5H9a.75.75 0 0 1-.75-.75ZM6 18a.75.75 0 0 1 .75-.75h.008a.75.75 0 0 1 .75.75v.008a.75.75 0 0 1-.75.75H6.75a.75.75 0 0 1-.75-.75V18Zm2.25 0a.75.75 0 0 1 .75-.75h3.75a.75.75 0 0 1 0 1.5H9a.75.75 0 0 1-.75-.75Z" clip-rule="evenodd" />
+        </svg>
+      </div>
+      <p>Students Who Evaluated</p>
+    </div>
+    <div class="flex pr-5 items-center">
+      <div class="flex-1">
+        <span id="total" class="text-lg font-semibold lg:text-xl">200</span>
+        <span id="label">Total</span>
+      </div>
+      <span id="arrow">
+        <svg class="w-7 h-7" data-slot="icon" fill="none" stroke-width="1.5" stroke="green" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 10.5 12 3m0 0 7.5 7.5M12 3v18"></path>
+        </svg>
+      </span>
     </div>
   </div>
 
